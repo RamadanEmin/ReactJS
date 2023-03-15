b075f7850584de830c63b69b8d2973fbe216ab72
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-03-15 12:47:21 +0200

    Initial project

---
+++ Components Basic Idea/react-movie-list/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+
+function App() {
+  const movie = [
+    {
+      title: 'Man of Steal',
+      year: 2008,
+      cast: ['Henry Cavil', 'Russell Crowe'],
+    },
+    {
+      title: 'Harry Potter',
+      year: 2008,
+      cast: ['Daniel Radclif', 'Emma Watson'],
+    },
+    {
+      title: 'Lord of the Rings',
+      year: 2008,
+      cast: ['Orlando Bloom', 'Russell Crowe'],
+    },
+  ];
+
+
+  return (
+    <div className="App">
+      <h1>React Demo</h1>
+
+    </div>
+  );
+}
+
+export default App;
