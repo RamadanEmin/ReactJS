87daa88e8671dc7a2f85ca68fddb1f47caa59e03
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-03-16 10:07:29 +0200

    Initial project

---
+++ Components Deep Dive/react-character-list/src/App.js
@@ -0,0 +1,15 @@
+import { CharacterList } from './components/CharacterList';
+import './App.css';
+
+function App() {
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <CharacterList />
+            </header>
+        </div>
+    );
+}
+
+export default App;
