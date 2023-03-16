0e54a05a5c126d61718767f4d7adc8415bf1a5dc
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-03-16 21:31:31 +0200

    Added Header, Loading and Todo list components

--- Components Deep Dive/react-todo-table/client/src/App.js
+++ Components Deep Dive/react-todo-table/client/src/App.js
@@ -1,5 +1,9 @@
 import { useEffect, useState } from 'react';
 
+import Loading from './components/Loading';
+import Header from './components/Header';
+import TodoList from './components/TodoList';
+
 function App() {
     const [todos, setTodos] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
@@ -14,8 +18,17 @@ function App() {
             });
     }, []);
 
+    const onTodoAdd = () => {
+        const lastId = Number(todos.length);
+        const text = prompt('Task name:');
+        const newTask = { id: lastId + 1, text, isCompleted: false };
+
+        setTodos(state => [newTask, ...state]);
+    };
+
     return (
         <div>
+            <Header />
 
             <main className="main">
 
@@ -27,7 +40,10 @@ function App() {
                     </div>
 
                     <div className="table-wrapper">
-                        
+                        {isLoading
+                            ? <Loading />
+                            : <TodoList todos={todos} toggleTodoStatus={toggleTodoStatus} />
+                        }
                     </div>
                 </section>
             </main>
