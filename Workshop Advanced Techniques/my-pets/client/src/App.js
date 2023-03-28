4fef2d2bd5bfb7a969dc4c42df69730b73a4d470
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-03-28 21:44:46 +0300

    Added Dashboard component

--- Workshop Advanced Techniques/my-pets/client/src/App.js
+++ Workshop Advanced Techniques/my-pets/client/src/App.js
@@ -1,10 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Dashboard from './components/Dashboard';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return ( 
-          <div id="container">     
+  return (
+          <div id="container">
+
             <main id="site-content">
-          
+              <Routes>
+                <Route path="/dashboard/*" element={<Dashboard />} />
+              </Routes>
             </main>
+
             <footer id="site-footer">
               <p>@PetMyPet</p>
             </footer>
---
+++ Workshop Advanced Techniques/my-pets/client/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+
+import * as petService from '../../services/petService';
+
+import './Dashboard.css';
+
+const Dashboard = () => {
+    const [pets, setPets] = useState([]);
+
+    useEffect(() => {
+        petService.getAll()
+            .then(result => {
+                setPets(result);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }, []);
+
+    return (
+        <section id="dashboard-page" className="dashboard">
+            <h1>Dashboard</h1>
+
+            <section>
+                <PetList pets={pets} />
+            </section>
+        </section>
+        
+    );
+}
+
+export default Dashboard;
