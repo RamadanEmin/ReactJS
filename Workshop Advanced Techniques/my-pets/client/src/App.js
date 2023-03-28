cd165b20435e0bea2d97833f8165412b615964f3
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-03-28 18:34:13 +0300

    Initial project and added useLocalStorage hook

---
+++ Workshop Advanced Techniques/my-pets/client/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return ( 
+          <div id="container">     
+            <main id="site-content">
+          
+            </main>
+            <footer id="site-footer">
+              <p>@PetMyPet</p>
+            </footer>
+          </div>
+  );
+}
+
+export default App;
---
+++ Workshop Advanced Techniques/my-pets/client/src/hooks/useLocalStorage.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+const useLocalStorage = (key, initialValue) => {
+    const [state, setState] = useState(() => {
+        try {
+            let item = localStorage.getItem(key);
+
+            return item 
+                ? JSON.parse(item) 
+                : initialValue;
+        } catch(err) {
+            console.log(err);
+            return initialValue;
+        }
+    });
+
+    const setItem = (value) => {
+        try {
+            localStorage.setItem(key, JSON.stringify(value))
+
+            setState(value);
+        } catch(err) {
+            console.log(err);
+        }
+    };
+
+    return [
+        state,
+        setItem
+    ];
+};
+
+export default useLocalStorage;
